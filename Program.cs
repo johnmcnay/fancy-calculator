de3bc768ff10e7e6dcb8f6238d3550442302b291
John McNay <John.McNay@improving.com>
2021-07-12 08:35:30 -0500

    refactored a bit

--- Program.cs
+++ Program.cs
@@ -8,52 +8,35 @@ namespace FancyCalculator
         {
             Console.WriteLine("A Console Calculator");
 
+            double x = getDoubleInput("Enter a number.");
+            double y = getDoubleInput("Enter a second number, and I will add it to the first.");
 
-
-            string num1;
-            double x;
-            bool loop = false;
-
-            do
-            {
-                Console.WriteLine("Enter a number.");
-                num1 = Console.ReadLine();
-
-                loop = !Double.TryParse(num1, out x);
-
-                if (loop)
-                {
-                    Console.WriteLine("Invalid Input");
-                }
-            } while (loop);
-
-
-            string num2;
-            double y;
-            loop = false;
-
-            do
-            {
-
-                Console.WriteLine("Enter a second number, and I will add it to the first.");
-                num2 = Console.ReadLine();
-
-                loop = !Double.TryParse(num2, out y);
-                if (loop)
-                {
-                    Console.WriteLine("Invalid Input");
-                }
-
-
-            } while (loop);
-
-            double result = double.Parse(num1) + double.Parse(num2);
+            double result = x + y;
 
             Console.WriteLine($"Result: {result}");
         }
+        public static double getDoubleInput(string prompt)
+        {
+            bool loop;
+            string input;
+            double result;
 
+            do
+            {
+                Console.WriteLine(prompt);
+                input = Console.ReadLine();
 
+                loop = !Double.TryParse(input, out result);
+                if (loop)
+                {
+                    Console.WriteLine("Invalid Input");
+                }
+            } while (loop);
 
-
+            return result;
+        }
     }
+
+
+
 }
