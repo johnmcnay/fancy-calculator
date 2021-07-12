4a4bd0c0323e2057b9b572909724293e26424a02
John McNay <John.McNay@improving.com>
2021-07-12 08:20:21 -0500

    step 1 complete

--- Program.cs
+++ Program.cs
@@ -6,7 +6,22 @@ namespace FancyCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("A Console Calculator");
+
+            Console.WriteLine("Enter a number.");
+
+            string num1 = Console.ReadLine();
+
+            Console.WriteLine("Enter a second number, and I will add it to the first.");
+
+            string num2 = Console.ReadLine();
+            double result = double.Parse(num1) + double.Parse(num2);
+
+            Console.WriteLine($"Result: {result}");
         }
+
+
+
+
     }
 }
