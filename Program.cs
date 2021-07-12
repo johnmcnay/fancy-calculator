51fa1edbed175c542139e6908d7cee61b66202e2
John McNay <John.McNay@improving.com>
2021-07-12 08:29:38 -0500

    step 2 complete

--- Program.cs
+++ Program.cs
@@ -8,13 +8,45 @@ namespace FancyCalculator
         {
             Console.WriteLine("A Console Calculator");
 
-            Console.WriteLine("Enter a number.");
 
-            string num1 = Console.ReadLine();
 
-            Console.WriteLine("Enter a second number, and I will add it to the first.");
+            string num1;
+            double x;
+            bool loop = false;
 
-            string num2 = Console.ReadLine();
+            do
+            {
+                Console.WriteLine("Enter a number.");
+                num1 = Console.ReadLine();
+
+                loop = !Double.TryParse(num1, out x);
+
+                if (loop)
+                {
+                    Console.WriteLine("Invalid Input");
+                }
+            } while (loop);
+
+
+            string num2;
+            double y;
+            loop = false;
+
+            do
+            {
+
+                Console.WriteLine("Enter a second number, and I will add it to the first.");
+                num2 = Console.ReadLine();
+
+                loop = !Double.TryParse(num2, out y);
+                if (loop)
+                {
+                    Console.WriteLine("Invalid Input");
+                }
+
+
+            } while (loop);
+
             double result = double.Parse(num1) + double.Parse(num2);
 
             Console.WriteLine($"Result: {result}");
